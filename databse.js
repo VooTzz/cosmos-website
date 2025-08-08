0d116c7a6d56765305173a01fc4fefaddaca94a3
VooTzz <jsvioskevkk@gmail.com>
2025-08-08 18:16:33 +0700

    style.css

--- databse.js
+++
@@ -1,6 +0,0 @@
-// Simulasi database user manual
-module.exports = [
-  { username: "vios", password: "vios001" },
-  { username: "vioss", password: "vios002" },
-  { username: "xoss", password: "xoss003" }
-];
